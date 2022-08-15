91225568a5b944e84962404390311661c35eee32
BlockJayn <blockjayn@gmail.com>
2022-08-15 15:16:29 +0200

    Revert "Merge commit '0e4a0d1b9bd75d55de97d8f751980dfd3b466571'"
    This reverts commit 9408d5c3d45a4c23ea443aff7b57f3ea01b735b8.

--- src/runner/duel-runner.js
+++ src/runner/duel-runner.js
@@ -18,123 +18,30 @@ const axios = require("axios");
 const ethers = require("ethers");
 const config = require("../config.json");
 const abiduel = require("./abi-duel.json");
-let provider, duelContract, wallet, current_rpc, pendingHeroInLobby, duelToComplete, currentBlockNr;
+let provider, duelContract, wallet;
 let receipt = { status: null }
 
 // RPC - switch automatically between harmonyRpc & poktRpc
-let rpc_auto_switch = config.rpc.rpc_auto_switch    // true: switch network on errors automatically | false: take standard settings from config
-let start_rpc = config.rpc.harmonyRpc               // Start with Main-RPC
+let rpc_auto_switch = true               // true: switch network on errors automatically | false: take standard settings from config
+let start_rpc = config.rpc.harmonyRpc    // Start with Main-RPC
+let current_rpc
 
 // Hero & Game Data
-const seedPassword = config.wallet.seedPassword                             //  Password for encrypting your seedphrase
-const type = config.game.gameType                                           //  Duel Game-Type = 1, 3 or 9
-const heroid = config.game.heroid                                           //  Duel Hero ID(s), like [ '260714', '154849', '30932' ]
-const jewelfee = config.game.jewelfee                                       //  Duel Jewel-Fee in HEX = 100000000000000000
-const background = config.game.background                                   //  Duel Background
-const stat = config.game.stat                                               //  Duel Stat
-const activateAutoCompleteDuel = config.game.activateAutoCompleteDuel       //  Automatically Completes Duels
+const seedPassword = 'fuckthis'             //  Password for encrypting your seedphrase
+const type = 1                              //  Duel Game-Type = 1, 3 or 9
+const heroid = [76568]                      //  Duel Hero ID(s), like [ '260714', '154849', '30932' ]
+const jewelfee = '0x016345785d8a0000'       //  Duel Jewel-Fee in HEX = 100000000000000000
+const background = 4                        //  Duel Background
+const stat = 0                              //  Duel Stat
 
 // Timer - in seconds
-const waitTimeAfterSuccess = config.timers.waitTimeAfterSuccess                 // Wait till next try after Success
-const waitTimeAfterError = config.timers.waitTimeAfterError                     // Wait till next try after Error
-const waitTimeAfterTXFail = config.timers.waitTimeAfterTXFail                   // Wait till next try after Error
-const transaction_timeout = config.timers.transaction_timeout                   // How long to wait, till Timeout
-const waitTimeLobby = config.timers.waitTimeLobby                               // How long to wait to check again, if Hero is still in Lobby without a Match
-const waitBlockTimeToAutocomplete = config.timers.waitBlockTimeToAutocomplete   // How many Blocks to wait till Duel gets Auto-Completed
+const waitTimeAfterSuccess = 120    // Wait till next try after Success
+const waitTimeAfterError = 120      // Wait till next try after Error
+const waitTimeAfterTXFail = 10      // Wait till next try after Error
+const transaction_timeout = 70      // How long to wait, till Timeout
 
 /*****************************************************************/
 
-
-
-async function getPlayerDuelEntries(walletaddress) {
-
-    // Open Duels in Lobby without a match, shows also Lobby ID
-    //   function getPlayerDuelEntries(address _profile) view returns (tuple(uint256 id, address player, uint256[] heroes, uint256 startBlock, uint256 score, uint256 scoreAfter, uint256 jewelFee, uint256 duelId, uint256 custom1, uint256 custom2, uint8 duelType, uint8 status)[]);
-    const getPlayerDuelEntries = await duelContract.getPlayerDuelEntries(walletaddress)
-    console.log('\n🔎 Checking Lobby for Address: ' + walletaddress + '...')
-    // console.log(getPlayerDuelEntries)
-    // console.log('getPlayerDuelEntries: ' + getPlayerDuelEntries)
-
-    // if array not empty = no active duel
-    if (getPlayerDuelEntries.length !== 0) {
-
-        let playerDuelEntries = getPlayerDuelEntries[0]
-
-        console.log(`
-        🙋‍♂️‍ Found Hero in Lobby waiting for a Match:
-            Lobby ID: ${playerDuelEntries[0]}
-            Player Address: ${playerDuelEntries[1]}
-            Hero IDs: ${playerDuelEntries[2]}
-            StartBlock: ${playerDuelEntries[3]}
-            Score: ${playerDuelEntries[4]}
-            Score After: ${playerDuelEntries[5]}
-            JewelFee: ${playerDuelEntries[6]}
-            Duel ID: ${playerDuelEntries[7]}
-            Background: ${playerDuelEntries[8]}
-            Stat: ${playerDuelEntries[9]}
-            Duel Type: ${playerDuelEntries[10]}
-            Status: ${playerDuelEntries[11]}
-            `)
-        /*
-        for (let index = 0; index < playerDuelEntries.length; index++) {
-            console.log("index:" + index + ":")
-            console.log(playerDuelEntries[index])
-        */
-
-        return playerDuelEntries[0]
-    }
-    else {
-        console.log('❌ No Active Lobby-Entries (Currently no Hero in Lobby-Queue)\n')
-    }
-
-}
-
-
-async function getActiveDuels(walletaddress) {
-
-    // Active Duels = Ready to fight - Array is empty when no active duels (waiting for a match)
-    //   function getActiveDuels(address _address) view returns (tuple(uint256 id, address player1, address player2, uint256 player1DuelEntry, uint256 player2DuelEntry, address winner, uint256[] player1Heroes, uint256[] player2Heroes, uint256 startBlock, uint8 duelType, uint8 status)[]);
-    const getActiveDuels = await duelContract.getActiveDuels(walletaddress)
-    console.log('🔎 Checking Active Duels for Address: ' + walletaddress + '...')
-    // console.log(getActiveDuels)
-    // console.log('Active Duels: ' + getActiveDuels)
-
-    // if array not empty = no active duel
-    if (getActiveDuels.length !== 0) {
-
-        let activeDuels = getActiveDuels[0]
-
-        console.log(`
-        🤼‍♂ Found Active Duel:
-            Duel ID: ${activeDuels[0]}
-            Player 1 Address: ${activeDuels[1]}
-            Player 2 Address: ${activeDuels[2]}
-            Player 1 Lobby ID: ${activeDuels[3]}
-            Player 2 Lobby ID: ${activeDuels[4]}
-            Winner Address: ${activeDuels[5]}
-            Player 1 Heroes: ${activeDuels[6]}
-            Player 2 Heroes: ${activeDuels[7]}
-            StartBlock: ${activeDuels[8]}
-            Duel Type: ${activeDuels[9]}
-            Status: ${activeDuels[10]}
-        `)
-        /*
-        for (let index = 0; index < activeDuels.length; index++) {
-            console.log("index:" + index + ":")
-            console.log(activeDuels[index])
-        }
-        */
-
-        return {
-            duelID: activeDuels[0],
-            startBlock: activeDuels[8]
-        }
-    }
-    else {
-        console.log('❌ No Active Duels (No Duels Ready for Battle / No Matches)\n')
-    }
-
-}
 
 
 // Function: RPC Network / Provider
@@ -171,7 +78,7 @@ function getRpc() {
     }
     else {  // use standard settings from config-file
 
-        let standardNet = config.rpc.useBackupRpc ? config.rpc.poktRpc : config.rpc.harmonyRpc;
+        let standardNet = config.useBackupRpc ? config.rpc.poktRpc : config.rpc.harmonyRpc;
         console.log('-----------------------------------------------------')
         console.log('🌐 Using Network: ' + standardNet + '\n')
 
@@ -201,43 +108,18 @@ async function start() {
             provider
         );
 
+
+        // NOT WORKING CURRNTLY - FIX THIS
+
         wallet = await fs.existsSync(config.wallet.encryptedWalletPath)
             ? await getEncryptedWallet()
             : await createWallet();
 
-
-        // If AutoCompleteDuel is activated, check for active Duel and Complete
-
-        if (activateAutoCompleteDuel) {
-            duelToComplete = await getActiveDuels(config.wallet.address)
-
-            // If there is an active Duel
-            if (duelToComplete != undefined) {
-
-                currentBlockNr = await provider.getBlockNumber()
-                blockNrTrigger = Number(duelToComplete.startBlock._hex) + waitBlockTimeToAutocomplete
-
-                if (currentBlockNr > blockNrTrigger) {
-                    await autoCompleteActiveDuel(duelToComplete.duelID)
-                }
-                else {
-                    console.log(`⏱‍  Auto-Completing Duel ${duelToComplete.duelID} delayed to save some fees.\n`);
-                }
-            }
-        }
+        //wallet = await getEncryptedWallet();
 
 
-        // Check the Lobby for pending Duels
 
-        pendingHeroInLobby = await getPlayerDuelEntries(config.wallet.address)
-
-        // If no Hero is in Lobby, send Hero to Lobby
-        if (pendingHeroInLobby === undefined) {
-            sendToDuel();   // Getting ready
-        }
-        else {  // Hero is already in Lobby, so we wait till Match is completed
-            consoleCountdown(waitTimeLobby, "Hero is already in Lobby waiting for a Match, let's wait...", 'restart')
-        }
+        sendToDuel();   // Getting ready
 
     } catch (err) {
 
@@ -345,67 +227,6 @@ async function consoleCountdown(timeToWait, message, action) {
 }
 
 
-
-// Function: AutoComplete Active Duel
-
-async function autoCompleteActiveDuel(duelId) {
-
-    try {
-        console.log(`🤼‍♂‍  Trying to complete Active Duel ${duelId}...\n`);
-
-        await tryTransactionAutocompleteDuel(
-            duelContract
-                .connect(wallet)
-                .completeDuel(duelId)
-        )
-
-    } catch (err) {
-
-        console.warn('⚠️  Error completing duel');
-        //console.log(err)
-
-    }
-}
-
-
-// Function: Try Transaction for Autocompleting Duel
-
-async function tryTransactionAutocompleteDuel(transaction) {
-
-    // If Transaction takes too long, exit Script and Restart
-
-    const timeout = setTimeout(() => {
-        console.log("⌛ Transaction Timeout. (tryTransactionAutocompleteDuel)");
-        //process.exit()
-    }, transaction_timeout * 1000)
-
-
-    try {
-
-        let tx = await transaction;
-        receipt = await tx.wait();
-
-        if (receipt.status === 1) {
-            console.log("✔️  Duel automatically completed!");
-            console.log('🌐 Tx: ' + receipt.transactionHash + ', Block: ' + receipt.blockNumber + '\n')
-        }
-        if (receipt.status !== 1) {
-            console.log("❌ Receipt threw an error.");
-            throw new Error(`Receipt had a status of ${receipt.status}`);
-        }
-
-        clearTimeout(timeout);
-
-    } catch (err) {
-
-        clearTimeout(timeout);
-
-        console.log("❌ Error broadcasting transaction for autocompleting Duel.\n");
-
-    }
-}
-
-
 // Function: Send Hero to Duel
 
 async function sendToDuel() {
@@ -414,9 +235,8 @@ async function sendToDuel() {
     const herodata = await getHeroMetaData(heroid[0]);   // console.log(herodata)
 
 
-
     try {
-        console.log(`⚔️  Sending ${herodata.firstname_string} ${herodata.lastname_string} to Duel-Lobby...\n`);
+        console.log(`⚔️  Trying to start a Duel with ${herodata.firstname_string} ${herodata.lastname_string}...\n`);
 
         await tryTransaction(
             duelContract
@@ -442,14 +262,14 @@ async function sendToDuel() {
 }
 
 
-// Function: Try Transaction for sending Hero to Duel
+// Function: Send Hero to Duel
 
 async function tryTransaction(transaction) {
 
     // If Transaction takes too long, exit Script and Restart
 
     const timeout = setTimeout(() => {
-        console.log("⌛ Transaction Timeout. (tryTransaction)");
+        console.log("⌛ Transaction Timeout.");
         process.exit()
     }, transaction_timeout * 1000)
 
@@ -479,13 +299,13 @@ async function tryTransaction(transaction) {
 
 
         if (err.message.includes('UNPREDICTABLE_GAS_LIMIT')) {
-            console.log("⚠️  There is a Network-Problem, or you don't have enough ONE/JEWEL/GOLD in your Wallet)\n");
+            console.log("⚠️  Hero is already in Duel-Queue (or Network-Problem | no ONE/JEWEL/GOLD in Wallet)\n");
 
             /* Error Transaction - Start again after a while */
             consoleCountdown(waitTimeAfterError, "Let's try again", 'restart')
         }
         else {
-            console.log("❌ Error broadcasting transaction for sending Hero to Duel Lobby.\n");
+            console.log("❌ Error broadcasting transaction.\n");
 
             /* Error Transaction - Start again after a while */
             consoleCountdown(waitTimeAfterTXFail, "Let's try again", 'restart')
